ff9bff6cb0cbb55011d7a09312b8f2383a46aebf
devjoe87 <63867164+devjoe87@users.noreply.github.com>
2020-11-03 12:31:53 +0000

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-  var user = "josephbarrett4"; /* treehouse username*/
   var num = 12; /* number of badges to display, starting w/ most recent */
 
-  $.getJSON("https://teamtreehouse.com/" + user + ".json", function (data) {
+  $.getJSON("https://teamtreehouse.com/josephbarrett4.json", function (data) {
     var output = "<ul>";
     /* array starts from 0, so you access the 10th element at position 9, add 1 to stop to account for the array starting from 0 */
     var i = data.badges.length - 1;
@@ -16,4 +15,31 @@ $(document).ready(function () {
     output += "</ul>";
     document.getElementById("badges").innerHTML = output;
   });
+});
+
+
+//-------------------------------------------------------------------------
+//--Points update
+//-------------------------------------------------------------------------
+
+$.getJSON("https://teamtreehouse.com/josephbarrett4.json", function(data){
+  var totalpts = data.points.total;
+  var jspts = data.points.JavaScript;
+  var csspts = data.points.CSS;
+  var htmlpts = data.points.HTML;
+  var rubypts = data.points.Ruby;
+  var devtoolpts = data.points["Development Tools"];
+  var designpts = data.points.Design;
+  var phppts = data.points.PHP;
+  var diglitpts = data.points["Digital Literacy"];
+  
+  $('.totalpts').append(totalpts);
+  $('.jspts').append(jspts);
+  $('.csspts').append(csspts);
+  $('.htmlpts').append(htmlpts);
+  $('.rubypts').append(rubypts);
+  $('.devtoolpts').append(devtoolpts);
+  $('.designpts').append(designpts);
+  $('.phppts').append(phppts);
+  $('.diglitpts').append(diglitpts);
 });
